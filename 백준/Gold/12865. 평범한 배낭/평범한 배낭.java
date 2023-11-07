af95c17119acdb33a3106a7edf75f32f080b2e21
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2023-11-07 18:31:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,27 +6,27 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] W = new int[N + 1]; // 무게
-        int[] V = new int[N + 1]; // 가치
-        int[] dp = new int[K + 1];
-
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+        int[] bag = new int[K + 1];
         for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             W[i] = Integer.parseInt(st.nextToken());
             V[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= N; i++) {
-            for(int j = K; j - W[i] >= 0; j--) {
-                dp[j] = Math.max(dp[j], dp[j - W[i]] + V[i]);
+            for (int j = K; j >= W[i]; j--) {
+                bag[j] = Math.max(bag[j], bag[j - W[i]] + V[i]);
             }
         }
-        System.out.println(dp[K]);
+
+        System.out.println(bag[K]);
     }
 }
