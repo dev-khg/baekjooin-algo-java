af677c4487e7b0f072bda454c8be70c2968950c2
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2023-11-22 17:09:10 +0900

    [Silver III] Title: 주유소, Time: 428 ms, Memory: 36448 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,25 +8,25 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] roads = new int[N - 1];
-        int[] stations = new int[N];
+        long[] distance = new long[N - 1];
+        long[] cost = new long[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N - 1; i++) {
-            roads[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N - 1; i++) {
+            distance[i] = Long.parseLong(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            stations[i] = Integer.parseInt(st.nextToken());
+            cost[i] = Long.parseLong(st.nextToken());
         }
 
-        int min = stations[0];
-        int costTotal = stations[0] * roads[0];
-
-        for(int i = 1; i < roads.length; i++) {
-            min = Math.min(min, stations[i]);
-            costTotal += min * roads[i];
+        long minValue = Long.MAX_VALUE;
+        long total = 0;
+        for (int i = 0; i < N - 1; i++) {
+            minValue = Math.min(minValue, cost[i]);
+            total += distance[i] * minValue;
         }
 
-        System.out.println(costTotal);
+        System.out.println(total);
     }
 }
