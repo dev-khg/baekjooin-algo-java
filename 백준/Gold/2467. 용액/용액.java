9701d363b64eeee25b61cbb39d29ac6b710a195e
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2023-11-29 19:49:14 +0900

    [Gold V] Title: 용액, Time: 388 ms, Memory: 28580 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -9,32 +9,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int answer = Integer.MAX_VALUE;
-
-        int lt = 0, rt = arr.length - 1;
-        int A = 0, B = 0;
+        int lt = 0, rt = N - 1;
+        int min = Integer.MAX_VALUE;
+        int[] answer = new int[2];
         while (lt < rt) {
-            int abs = Math.abs(arr[lt] + arr[rt]);
             int sum = arr[lt] + arr[rt];
-
-            if(abs < answer) {
-                answer = abs;
-                A = arr[lt];
-                B = arr[rt];
+            if(min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                answer[0] = arr[lt];
+                answer[1] = arr[rt];
             }
 
-            if(sum < 0) lt++;
-            else rt--;
+            if(sum < 0) {
+                lt++;
+            } else rt--;
         }
 
-        System.out.println(A + " " + B);
+        System.out.println(answer[0] + " " + answer[1]);
     }
 }
