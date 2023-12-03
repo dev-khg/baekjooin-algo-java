d6907ec441fbcaab0ecfcd6692099973ea8def96
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2023-12-04 00:14:23 +0900

    [Gold V] Title: 탑, Time: 748 ms, Memory: 81396 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -2,33 +2,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
         int[] arr = new int[N + 1];
-        int[] answer = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Stack<Integer> stack = new Stack<>();
-        for (int i = arr.length - 1; i > 0; i--) {
+        int[] rank = new int[N + 1];
+        for (int i = N; i >= 1; i--) {
             while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                Integer pop = stack.pop();
-                answer[pop] = i;
+                rank[stack.pop()] = i;
             }
             stack.push(i);
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            System.out.print(answer[i] + " ");
+            sb.append(rank[i]).append(" ");
         }
+
+        System.out.println(sb);
     }
 }
