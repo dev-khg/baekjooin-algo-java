51e7a5c021f1d3cb7d9b3646e5ea20fd96a77d1a
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2023-12-25 19:52:48 +0900

    [Bronze I] Title: 소인수분해, Time: 160 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            while (N % i == 0) {
+                sb.append(i).append("\n");
+                N /= i;
+            }
+            if(N == 1) break;
+        }
+
+        System.out.println(sb);
+    }
+}
