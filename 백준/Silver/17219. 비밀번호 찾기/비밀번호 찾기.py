c8245dc398b6f7a955fa4469ada76271bcd68610
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-05 09:52:17 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 360 ms, Memory: 135172 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,11 +1,17 @@
+import sys
 
-domain_password_dict = dict()
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
+domain_password_dict = dict()
 
 for _ in range(N):
     domain, password = input().split()
     domain_password_dict[domain] = password
 
 for _ in range(M):
-    print(domain_password_dict[input()])
+    domain = input().rstrip()
+    if domain in domain_password_dict:
+        print(domain_password_dict[domain])
+
+
