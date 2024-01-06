c0a190a8b04d145cb3cd206b57ffd5e4005fa7e5
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-06 21:04:20 +0900

    [Gold IV] Title: 세 수의 합, Time: 232 ms, Memory: 190636 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+nums = [int(input()) for _ in range(N)]
+nums.sort()
+
+sum_set = set()
+for i in nums:
+    for j in nums:
+        sum_set.add(i + j)
+
+
+def solve():
+    for i in range(N - 1, -1, -1):
+        for j in range(i + 1):
+            if nums[i] - nums[j] in sum_set:
+                return nums[i]
+
+print(solve())
