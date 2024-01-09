2913ebd38bc2e55b5abb78fee98479b622bdcfe3
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-09 20:57:11 +0900

    [Gold I] Title: 냅색문제, Time: 148 ms, Memory: 113292 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.py
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -1,44 +1,36 @@
+import bisect
+import sys
+
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+w = list(map(int, input().split()))
+
+a_w = w[:n // 2]
+b_w = w[n // 2:]
+a_sum = []
+b_sum = []
 
 
-N, C = map(int, input().split())
-sumA = []
-sumB = []
-
-nums = [int(s) for s in input().split()]
-A_W = nums[:N // 2]
-B_W = nums[N // 2:]
-
-
-def dfs(warr, sum_arr, l, w):
-    if l >= len(warr):
-        sum_arr.append(w)
+def brute_force(L: int, total: int, weights:list[int], sum_arr: list[int]) -> None:
+    if L == len(weights):
+        sum_arr.append(total)
         return
-    dfs(warr, sum_arr, l + 1, w)
-    dfs(warr, sum_arr, l + 1, w + warr[l])
+
+    brute_force(L + 1, total, weights, sum_arr)
+    brute_force(L + 1, total + weights[L], weights, sum_arr)
 
 
-def binary_search(arr, target):
-    lt, rt = 0, len(arr)
-    while lt < rt:
-        mid = (lt + rt) // 2
-        if arr[mid] <= target:
-            lt = mid + 1
-        else:
-            rt = mid
-    return rt
-
-dfs(A_W, sumA, 0, 0)
-dfs(B_W, sumB, 0, 0)
-sumB.sort()
+brute_force(0, 0, a_w, a_sum)
+brute_force(0, 0, b_w, b_sum)
+b_sum.sort()
 answer = 0
 
-
-
-for i in sumA:
-    if C - i < 0:
+for a in a_sum:
+    if c - a < 0:
         continue
-    answer += binary_search(sumB, C - i)
+
+    idx = bisect.bisect_right(b_sum, c - a)
+    answer += idx
 
 print(answer)
-
-
