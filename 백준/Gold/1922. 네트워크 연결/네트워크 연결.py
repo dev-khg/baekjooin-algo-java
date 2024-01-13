29ff9711c59cc590b067d96ef37f8055cc4d4d82
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-13 13:24:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 304 ms, Memory: 129500 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -4,31 +4,45 @@ import sys
 
 input = sys.stdin.readline
 
+class DisjointSet:
+    def __init__(self, n):
+        self.parents = list(range(n + 1))
+
+
+    def find(self, v):
+        if self.parents[v] == v: return v
+        self.parents[v] = self.find(self.parents[v])
+        return self.parents[v]
+
+    def union(self, v1, v2) -> bool:
+        root_v1 = self.find(v1)
+        root_v2 = self.find(v2)
+
+        if root_v1 != root_v2:
+            self.parents[root_v1] = root_v2
+            return True
+        return False
+
+
 N = int(input())
 M = int(input())
-
-graph = collections.defaultdict(list)
-visited = [False] * 1001
+disjoint_set = DisjointSet(N)
+answer = 0
+selected = 0
+min_heap = []
 
 for _ in range(M):
-    A, B, cost = map(int, input().split())
-    graph[A].append((B, cost))
-    graph[B].append((A, cost))
+    v1, v2, cost = map(int, input().split())
+    min_heap.append((cost, v1, v2))
 
 
+heapq.heapify(min_heap)
 
-min_heap = []
-heapq.heappush(min_heap, (0, 1))
-answer = 0
-
-while min_heap:
-    cost, v = heapq.heappop(min_heap)
-    if visited[v]:
-        continue
-    visited[v] = True
-    answer += cost
-    for next_vertex, cost in graph[v]:
-        if not visited[next_vertex]:
-            heapq.heappush(min_heap, (cost, next_vertex))
+while selected < N - 1:
+    temp = heapq.heappop(min_heap)
+    cost, v1, v2 = temp[0], temp[1], temp[2]
+    if disjoint_set.union(v1, v2):
+        answer += cost
+        selected += 1
 
 print(answer)
