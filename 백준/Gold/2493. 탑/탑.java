a0f1c92a66264112170f5f3b72d30678edaca2f6
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-03-05 01:28:10 +0900

    [Gold V] Title: 탑, Time: 728 ms, Memory: 81384 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,32 +1,34 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> s = new Stack<>();
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
+        int[] heights = new int[N];
+        int[] answer = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
         }
-        Stack<Integer> stack = new Stack<>();
-        int[] rank = new int[N + 1];
-        for (int i = N; i >= 1; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                rank[stack.pop()] = i;
+
+        for (int idx = N - 1; idx >= 0; idx--) {
+            while (!s.isEmpty() && heights[idx] > heights[s.peek()]) {
+                answer[s.pop()] = idx + 1;
             }
-            stack.push(i);
+
+            s.add(idx);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            sb.append(rank[i]).append(" ");
+        for (int i = 0; i < answer.length; i++) {
+            sb.append(answer[i]).append(" ");
         }
 
         System.out.println(sb);
