8cbb0eaebb2b5274f25f07f751c4af2be3ad6421
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-03-15 14:07:39 +0900

    [Silver I] Title: 경로 찾기, Time: 240 ms, Memory: 35820 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,20 @@
+import collections
+import heapq
+import itertools
+import sys
+
+
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+
+for i in range(N):
+    print(*graph[i], sep=' ')
