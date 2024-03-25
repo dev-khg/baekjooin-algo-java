da8108059741359976ce1bf9b3718bb0c3626f43
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-03-25 12:51:41 +0900

    [Platinum III] Title: 효율적으로 소 사기, Time: 168 ms, Memory: 43944 KB -BaekjoonHub

--- 백준/Platinum/5896. 효율적으로 소 사기/효율적으로 소 사기.py
+++ 백준/Platinum/5896. 효율적으로 소 사기/효율적으로 소 사기.py
@@ -1,40 +1,35 @@
-import bisect
 import collections
 import heapq
 import sys
 
 input = sys.stdin.readline
 
-N, K, M = map(int, input().split())
+n, k, m = map(int, input().split())
 cows = []
 min_heap = []
 answer = 0
-total = 0
+spent_money = 0
 
-for _ in range(N):
+for _ in range(n):
     P, C = map(int, input().split())
     cows.append((P, C))
 
 cows.sort(key=lambda x:(x[1], x[0]))
 
-
-for i in range(K):
-    if cows[i][1] + total <= M:
-        total += cows[i][1]
+for i in range(k):
+    if cows[i][1] + spent_money <= m:
+        spent_money += cows[i][1]
         answer += 1
-        heapq.heappush(min_heap, cows[i][0] - cows[i][1])
+        heapq.heappush(min_heap, (cows[i][0] - cows[i][1]))
 
-
-for i in range(K, N):
+for i in range(k, n):
     if min_heap and min_heap[0] + cows[i][1] < cows[i][0]:
-        if total + min_heap[0] + cows[i][1] <= M:
-            total += heapq.heappop(min_heap) + cows[i][1]
+        if spent_money + min_heap[0] + cows[i][1] <= m:
+            spent_money += heapq.heappop(min_heap) + cows[i][1]
             answer += 1
-            heapq.heappush(min_heap, cows[i][0] - cows[i][1])
-    else:
-        if total + cows[i][0] <= M:
-            total += cows[i][0]
-            answer += 1
-
+            heapq.heappush(min_heap, (cows[i][0] - cows[i][1]))
+    elif spent_money + cows[i][0] <= m:
+        spent_money += cows[i][0]
+        answer += 1
 
 print(answer)
