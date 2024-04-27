358ab0dfc4915d53ab20934ecea8f0bbf2f39a14
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-04-27 13:25:27 +0900

    [level 2] Title: 구명보트, Time: 8.18 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         
-        Arrays.sort(people);
         int lt = 0, rt = people.length - 1;
-        
-        while(lt <= rt) {
-            if(people[lt] + people[rt] <= limit) {
+        Arrays.sort(people);
+
+        while (lt <= rt) {
+            if (people[lt] + people[rt] <= limit) {
                 lt++;
-            }
+            } 
             rt--;
-            answer++;            
+            answer++;
         }
         
         return answer;
